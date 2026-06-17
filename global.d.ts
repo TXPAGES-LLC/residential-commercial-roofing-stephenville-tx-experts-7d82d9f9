d4cabfa0c307c7c682e114a3df6395be6ed992f8
TXPAGES-LLC <mark@txpages.com>
2026-06-17 16:33:40 -0500

    Auto-fix attempt 2

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,5 @@
+// Allow importing global CSS files (and other style files) as side-effect imports in TypeScript
+// This prevents errors like: "Cannot find module or type declarations for side-effect import of './globals.css'"
 declare module '*.css';
 declare module '*.scss';
-declare module '*.module.css';
-declare module '*.module.scss';
-export {};
+declare module '*.sass';
